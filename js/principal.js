2d4ae16ec3b517c023cbe50f98d44f3bcad08b3f
Henrique Tiago Tyminski <tyminski2011@gmail.com>
2019-08-29 11:36:46 -0300

    1.0

---
+++ js/principal.js
@@ -0,0 +1,26 @@
+
+  //var titulo = document.querySelector(".titulo");
+  //titulo.textContent = "Banana";
+
+  var paciente = document.querySelector("#primeiro-paciente");
+  var tdPeso = paciente.querySelector(".info-peso");
+  
+  var peso = tdPeso.textContent;
+  
+  console.log(paciente); // tr
+  console.log(tdPeso);   // td peso
+  console.log(peso);     // Obter 100
+
+		//alert("Olá Mundo");
+		 //console.log("Oi Mundo");
+		 //no console - console.log("aqui tem um recado para vc");
+		 //document
+		 //console.log(document);
+     //console.log(document.querySelector("h1"));
+			// <h1> <p> <span> = text content
+			//console.log(titulo);//<h1> Aparecida </h1>
+			//console.log(titulo.textContent);//Aparecida
+
+			//var titulo = document.querySelector("h1");
+      //titulo.textContent = "Banana";
+      //DOM ou Document Object Model
